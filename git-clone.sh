3eafcccf43de55bd2f0e6295d6c8807be30f18f9
Tomonori Matsumura <matsumura_t@yahoo.co.jp>
2017-08-18 02:03:45 +0900

    first commit

---
+++ git-clone.sh
@@ -0,0 +1,1 @@
+git clone https://github.com/TomonoriMatsumura/ansible_joke-program.git
---
+++ git-config.sh
@@ -0,0 +1,2 @@
+git config --global user.name "Tomonori Matsumura"
+git config --global user.email matsumura_t@yahoo.co.jp
